48f76c08267de102a4fab9c8250c36d183f8fd48
Maruchero <marce.gnz303@gmail.com>
2025-11-10 19:39:53 +0000

    lved: remove grid snapping

--- src/level_editor.rs
+++ src/level_editor.rs
@@ -2,9 +2,8 @@
 use bevy::prelude::*;
 use bevy_egui::{egui, EguiContexts, EguiPlugin};
 
+use crate::asset_loading::CaveAtlases;
 use crate::states::GameState;
-
-const GRID_SIZE: f32 = 32.0;
 
 #[derive(Component)]
 pub struct Tile;
@@ -13,14 +12,14 @@ pub struct Tile;
 pub struct LevelEditorCamera;
 
 #[derive(Resource, Default, Debug)]
-pub struct SnappedCoordinates(pub Vec2);
+pub struct WorldCoordinates(pub Vec2);
 
 pub struct LevelEditorPlugin;
 
 impl Plugin for LevelEditorPlugin {
     fn build(&self, app: &mut App) {
         app.add_plugins(EguiPlugin)
-            .init_resource::<SnappedCoordinates>()
+            .init_resource::<WorldCoordinates>()
             .add_systems(OnEnter(GameState::LevelEditor), setup_level_editor)
             .add_systems(
                 Update,
@@ -76,7 +75,7 @@ fn editor_ui_system(
 }
 
 fn cursor_system(
-    mut snapped_coords: ResMut<SnappedCoordinates>,
+    mut world_coords: ResMut<WorldCoordinates>,
     q_window: Query<&Window>,
     q_camera: Query<(&Camera, &GlobalTransform), With<LevelEditorCamera>>,
 ) {
@@ -87,28 +86,26 @@ fn cursor_system(
         .and_then(|cursor| camera.viewport_to_world(camera_transform, cursor))
         .map(|ray| ray.origin.truncate())
     {
-        let snapped_x = (world_position.x / GRID_SIZE).round() * GRID_SIZE;
-        let snapped_y = (world_position.y / GRID_SIZE).round() * GRID_SIZE;
-
-        snapped_coords.0 = Vec2::new(snapped_x, snapped_y);
+        world_coords.0 = world_position;
     }
 }
 
 fn tile_placement_system(
     mut commands: Commands,
-    snapped_coords: Res<SnappedCoordinates>,
+    world_coords: Res<WorldCoordinates>,
     mouse_button_input: Res<ButtonInput<MouseButton>>,
-    q_tiles: Query<(Entity, &Transform), With<Tile>>,
+    q_tiles: Query<(Entity, &Transform, &Sprite), With<Tile>>,
+    cave_atlases: Res<CaveAtlases>,
 ) {
     if mouse_button_input.just_pressed(MouseButton::Left) {
         commands.spawn((
-            SpriteBundle {
-                sprite: Sprite {
-                    color: Color::rgb(0.5, 0.5, 0.5),
-                    custom_size: Some(Vec2::new(GRID_SIZE, GRID_SIZE)),
-                    ..default()
+            SpriteSheetBundle {
+                texture: cave_atlases.platform_image.clone(),
+                atlas: TextureAtlas {
+                    layout: cave_atlases.platform_atlas.clone(),
+                    index: 0, // Index of the 'small_square' sprite
                 },
-                transform: Transform::from_translation(snapped_coords.0.extend(0.0)),
+                transform: Transform::from_translation(world_coords.0.extend(0.0)),
                 ..default()
             },
             Tile,
@@ -116,8 +113,11 @@ fn tile_placement_system(
     }
 
     if mouse_button_input.just_pressed(MouseButton::Right) {
-        for (entity, transform) in q_tiles.iter() {
-            if transform.translation.truncate() == snapped_coords.0 {
+        for (entity, transform, sprite) in q_tiles.iter() {
+            let size = sprite.custom_size.unwrap_or(Vec2::new(1.0, 1.0));
+            let tile_rect = Rect::from_center_size(transform.translation.truncate(), size);
+
+            if tile_rect.contains(world_coords.0) {
                 commands.entity(entity).despawn();
             }
         }
